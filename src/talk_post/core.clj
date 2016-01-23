a4f5eab3cc1ae75953349a9a054dcdb4c07ec4ff
trikitrok <trikitrok@gmail.com>
2016-01-23 22:43:51 +0100

    Generating also post title

--- src/talk_post/core.clj
+++ src/talk_post/core.clj
@@ -1,5 +1,9 @@
 (ns talk-post.core
   (:require [selmer.parser :as parser]))
 
-(defn generate-html [talk]
-  (parser/render-file "talk_template.html" talk))
+(def generate-html
+  (partial parser/render-file "talk_template.html"))
+
+(defn generate-post [& {title :title :as talk}]
+  {:content (generate-html talk)
+   :title (parser/render "Interesting Talk: &quot;{{title}}&quot;" {:title title})})
--- test/talk_post/core_test.clj
+++ test/talk_post/core_test.clj
@@ -10,8 +10,20 @@
 
     (generate-html
       {:adjective "wonderful"
-       :author-url "http://nestorsalceda.com/blog/"
-       :author-name "Nestor Salceda"
-       :url "http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible"
-       :title "Despliegue continuo con Docker y Ansible"})
-    => "I've just watched this wonderful talk by <a href=\"http://nestorsalceda.com/blog/\">Nestor Salceda</a>:\n<ul>\n    <li>\n        <a href=\"http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible\">Despliegue continuo con Docker y Ansible</a>\n    </li>\n</ul>"))
+      :author-url "http://nestorsalceda.com/blog/"
+      :author-name "Nestor Salceda"
+      :url "http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible"
+      :title "Despliegue continuo con Docker y Ansible"})
+    => "I've just watched this wonderful talk by <a href=\"http://nestorsalceda.com/blog/\">Nestor Salceda</a>:\n<ul>\n    <li>\n        <a href=\"http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible\">Despliegue continuo con Docker y Ansible</a>\n    </li>\n</ul>")
+
+  (fact
+    "its generates its post"
+
+    (generate-post
+      :adjective "wonderful"
+      :author-url "http://nestorsalceda.com/blog/"
+      :author-name "Nestor Salceda"
+      :url "http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible"
+      :title "Despliegue continuo con Docker y Ansible")
+    => {:title "Interesting Talk: &quot;Despliegue continuo con Docker y Ansible&quot;"
+        :content "I've just watched this wonderful talk by <a href=\"http://nestorsalceda.com/blog/\">Nestor Salceda</a>:\n<ul>\n    <li>\n        <a href=\"http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible\">Despliegue continuo con Docker y Ansible</a>\n    </li>\n</ul>"}))
