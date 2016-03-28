8857ebc3a65560b360e90725ee6fb905898f1274
trikitrok <trikitrok@gmail.com>
2016-03-29 01:54:21 +0200

    Changed to work with a list of authors

--- test/talk_post/core_test.clj
+++ test/talk_post/core_test.clj
@@ -2,32 +2,36 @@
   (:require [midje.sweet :refer :all]
             [talk-post.core :refer :all]))
 
+(defn without-new-lines [s]
+  (clojure.string/replace s #"\n" ""))
+
 (facts
   "about posts"
 
   (fact
     "it generates a watched talk post"
 
-    (generate-talk-post
-      :adjective "wonderful"
-      :author-url "http://nestorsalceda.com/blog/"
-      :author-name "Nestor Salceda"
-      :url "http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible"
-      :title "Despliegue continuo con Docker y Ansible")
-    => {:title "Interesting Talk: &quot;Despliegue continuo con Docker y Ansible&quot;"
-        :content "I've just watched this wonderful talk by <a href='http://nestorsalceda.com/blog/'>Nestor Salceda</a>:\n<ul>\n    <li>\n        <a href='http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible'>Despliegue continuo con Docker y Ansible</a>\n    </li>\n</ul>"})
+    (let
+      [post (generate-talk-post
+              :adjective "wonderful"
+              :authors [{:url "http://nestorsalceda.com/blog/"
+                         :name "Nestor Salceda"}]
+              :url "http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible"
+              :title "Despliegue continuo con Docker y Ansible")]
+      (:title post) => "Interesting Talk: &quot;Despliegue continuo con Docker y Ansible&quot;"
+      (without-new-lines (:content post)) => "I've just watched this wonderful talk by<a href='http://nestorsalceda.com/blog/'>Nestor Salceda</a>:<ul>    <li>        <a href='http://www.decharlas.uji.es/es/despliegue-continuo-docker-ansible'>Despliegue continuo con Docker y Ansible</a>    </li></ul>"))
 
   (fact
     "it generates a read paper post"
 
-    (generate-paper-post
-      :adjective "great"
-      :author-url "https://en.wikipedia.org/wiki/Peter_Naur"
-      :author-name "Peter Naur"
-      :url "http://pages.cs.wisc.edu/~remzi/Naur.pdf"
-      :title "Programming as Theory Building")
-    => {:title "Interesting Paper: &quot;Programming as Theory Building&quot;"
-        :content "I've just read this great paper by <a href='https://en.wikipedia.org/wiki/Peter_Naur'>Peter Naur</a>:\n<ul>\n    <li>\n        <a href='http://pages.cs.wisc.edu/~remzi/Naur.pdf'>Programming as Theory Building</a>\n    </li>\n</ul>"})
+    (let [post (generate-paper-post
+                 :adjective "great"
+                 :authors [{:url "https://en.wikipedia.org/wiki/Peter_Naur"
+                            :name "Peter Naur"}]
+                 :url "http://pages.cs.wisc.edu/~remzi/Naur.pdf"
+                 :title "Programming as Theory Building")]
+      (:title post) =>"Interesting Paper: &quot;Programming as Theory Building&quot;"
+      (without-new-lines (:content post)) => "I've just read this great paper by<a href='https://en.wikipedia.org/wiki/Peter_Naur'>Peter Naur</a>:<ul>    <li>        <a href='http://pages.cs.wisc.edu/~remzi/Naur.pdf'>Programming as Theory Building</a>    </li></ul>"))
 
   (fact
     "it generates a listened to podcast post"
